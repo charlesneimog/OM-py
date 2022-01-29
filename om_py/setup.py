ad4c0d1461829fdfbba9da387be36689fc3b6176
Charles K. Neimog <charlesneimog@outlook.com>
2022-01-29 14:46:51 -0300

    update om_py to 0.0.0.4

--- om_py/setup.py
+++ om_py/setup.py
@@ -1,4 +1,5 @@
 # python setup.py sdist bdist_wheel
+# 
 from setuptools import setup, find_packages
 import codecs
 import os
@@ -8,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.0.3'
+VERSION = '0.0.0.4'
 DESCRIPTION = 'This is a simple package to help musicians that do not use a lot of code. '
 LONG_DESCRIPTION = 'CHARLESNEIMOG.COM'
 
