08b01bdeb34850b61f0d16ecdb26b8ceece733a9
Charles K. Neimog <charlesneimog@outlook.com>
2022-02-07 19:42:01 -0300

    add first-load to not always reinstall venv, om_py and others

--- Sources/from om_py import to_om.py
+++
@@ -1,4 +0,0 @@
-from om_py import to_om
-
-
-to_om(r'C:\Users\charl\OneDrive_usp.br\Documents\OpenMusic\out-files\Musicxml teste.musicxml')
--- Sources/import modules.py
+++
@@ -1,3 +0,0 @@
-from om_py import to_om
-import vispy
-from math import sum
